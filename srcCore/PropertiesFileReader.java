c13b5015c39a237a78f2430fbcc8efe912059c08
Kien Seng <kienseng.koh@jobstreet.com>
2016-03-22 18:14:35 +0800

    s

--- srcCore/PropertiesFileReader.java
+++ srcCore/PropertiesFileReader.java
@@ -1,5 +1,24 @@
+import sun.reflect.annotation.ExceptionProxy;
+
+import java.io.File;
+import java.util.Properties;
+
 /**
  * Created by kienseng.koh on 3/22/2016.
  */
 public class PropertiesFileReader {
+    Properties prop = new Properties();
+    File file = new File("config.properties");
+
+    public String getPropertyByKey() throws Exception{
+
+    }
+
+    public int getPropertyByKey() throws Exception {
+
+    }
+
+    private void closeFile() throws Exception{
+
+    }
 }
