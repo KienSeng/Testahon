1c61dcb0a137aaaa8b663cad4291cde60f990677
Kien Seng <kienseng.koh@jobstreet.com>
2016-04-13 16:51:06 +0800

    d

--- srcInterface/userInterface/TAScenariosReviewer.java
+++ srcInterface/userInterface/TAScenariosReviewer.java
@@ -6,10 +6,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.*;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 
 import java.net.URL;
@@ -47,45 +44,60 @@ public class TAScenariosReviewer implements Initializable {
         txt_searchByTestClass = new TextField();
         lbl_searchFromList = new Label();
         lbl_searchByTestClass = new Label();
+        Separator spr_SearchContent = new Separator();
+        HBox layout_Hbox_SearchButton = new HBox();
+        Button btn_search = new Button();
+        Button btn_clear = new Button();
 
         layout_FlowPane_Main.setPadding(new Insets(15,15,15,15));
-        layout_FlowPane_Main.setOrientation(Orientation.VERTICAL);
         layout_FlowPane_Main.setRowValignment(VPos.TOP);
-//        layout_FlowPane_Main.setColumnHalignment(HPos.LEFT);
-        layout_FlowPane_Main.setStyle("-fx-background-color: red;");
-        layout_FlowPane_Main.setPrefWidth(Double.MAX_VALUE);
 
-        layout_FlowPane_Main.setPadding(new Insets(5,5,5,5));
         layout_FlowPane_SearchSection.setOrientation(Orientation.HORIZONTAL);
         layout_FlowPane_SearchSection.setRowValignment(VPos.TOP);
-        layout_FlowPane_SearchSection.setHgap(100);
+        layout_FlowPane_SearchSection.minWidthProperty().bind(layout_FlowPane_Main.widthProperty());
+        layout_FlowPane_SearchSection.setHgap(350);
         layout_FlowPane_SearchSection.setVgap(20);
-//        layout_FlowPane_SearchSection.setStyle("-fx-background-color: blue;");
-//        layout_FlowPane_SearchSection.minWidthProperty().bind(layout_FlowPane_Main.widthProperty());
 
         layout_Hbox_SearchContentList.setSpacing(20);
-        layout_Hbox_SearchContentList.setMinWidth(Global.standardLabelWidth + Global.standardTextBoxWidth + 80);
-//        layout_Hbox_SearchContentList.setAlignment(Pos.CENTER_LEFT);
-        layout_Hbox_SearchContentList.setStyle("-fx-background-color: blue;");
+        layout_Hbox_SearchContentList.setMinWidth(Global.standardLabelWidth + Global.standardTextBoxWidth);
 
         layout_Hbox_SearchContentTextBox.setSpacing(20);
-        layout_Hbox_SearchContentTextBox.setMinWidth(Global.standardLabelWidth + Global.standardTextBoxWidth + 80);
-//        layout_Hbox_SearchContentTextBox.setAlignment(Pos.CENTER_LEFT);
-        layout_Hbox_SearchContentTextBox.setStyle("-fx-background-color: yellow;");
+        layout_Hbox_SearchContentTextBox.setMinWidth(Global.standardLabelWidth + Global.standardTextBoxWidth);
+
+        layout_Hbox_SearchButton.setPadding(new Insets(15,0,15,0));
+        layout_Hbox_SearchButton.spacingProperty().bind(layout_FlowPane_Main.widthProperty().divide(6));
+        layout_Hbox_SearchButton.setAlignment(Pos.CENTER);
+        layout_Hbox_SearchButton.minWidthProperty().bind(layout_FlowPane_Main.widthProperty());
 
         lbl_searchFromList.setText("Search from list:");
         lbl_searchByTestClass.setText("Search by Test Class:");
 
-        lbl_searchFromList.setPrefWidth(Global.standardLabelWidth);
-        lbl_searchByTestClass.setPrefWidth(Global.standardLabelWidth);
-        cmb_searchFromList.setPrefWidth(Global.standardTextBoxWidth);
-        txt_searchByTestClass.setPrefWidth(Global.standardTextBoxWidth);
+        lbl_searchFromList.setPrefWidth(Global.standardLabelWidth + 50);
+        lbl_searchByTestClass.setPrefWidth(Global.standardLabelWidth + 50);
+        cmb_searchFromList.setPrefWidth(Global.standardTextBoxWidth + 50);
+        txt_searchByTestClass.setPrefWidth(Global.standardTextBoxWidth + 50);
+
+        btn_search.setId("btn_search");
+        btn_search.setText("Search");
+        btn_search.setMinWidth(Global.standardButtonWidth);
+        btn_search.setAlignment(Pos.CENTER);
+
+        btn_clear.setId("btn_clear");
+        btn_clear.setText("Clear");
+        btn_clear.setMinWidth(Global.standardButtonWidth);
+        btn_clear.setAlignment(Pos.CENTER);
+
+        spr_SearchContent.setOrientation(Orientation.HORIZONTAL);
+        spr_SearchContent.setPadding(new Insets(0,30,0,0));
+        spr_SearchContent.minWidthProperty().bind(layout_FlowPane_Main.widthProperty());
 
         layout_Hbox_SearchContentList.getChildren().addAll(lbl_searchFromList, cmb_searchFromList);
         layout_Hbox_SearchContentTextBox.getChildren().addAll(lbl_searchByTestClass, txt_searchByTestClass);
+        layout_Hbox_SearchButton.getChildren().addAll(btn_search, btn_clear);
         layout_FlowPane_SearchSection.getChildren().addAll(layout_Hbox_SearchContentList, layout_Hbox_SearchContentTextBox);
-        layout_FlowPane_Main.getChildren().add(layout_FlowPane_SearchSection);
-
+        layout_FlowPane_Main.getChildren().add(0, layout_FlowPane_SearchSection);
+        layout_FlowPane_Main.getChildren().add(1, layout_Hbox_SearchButton);
+        layout_FlowPane_Main.getChildren().add(2, spr_SearchContent);
     }
 
     private void getAllTestClassFromDb() throws Exception{
